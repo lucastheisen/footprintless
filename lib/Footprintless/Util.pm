36ad498b4b196b78e20f6e51795d158a7f62db74
Lucas Theisen <ltheisen@mitre.org>
2016-03-23 17:59:03 -0400

    added pod for Footprintless::Util

--- lib/Footprintless/Util.pm
+++ lib/Footprintless/Util.pm
@@ -3,6 +3,9 @@ use warnings;
 
 package Footprintless::Util;
 
+# ABSTRACT: A utility method package for common functionality in Footprintless
+# PODNAME: Footprintless::Util
+
 use Carp;
 use Exporter qw(import);
 
@@ -63,3 +66,69 @@ sub spurt {
 }
 
 1;
+
+__END__
+=head1 SYNOPSIS
+
+  use Footprintless::Util qw(
+      agent
+      default_command_runner
+      dumper
+      exit_due_to
+      slurp
+      spurt
+  );
+
+  my $agent = agent();
+  my $command_runner = default_command_runner();
+  my $dumper = dumper();
+  
+  eval {
+      $command_runner->run_or_die('cat /foo/bar');
+  };
+  exit_due_to($@) if ($@);
+
+  my $content = slurp('/foo/bar');
+  spurt('baz', '/foo/bar', append => 1);
+
+=head1 DESCRIPTION
+
+This module contains common utility methods used by Footprintless.
+
+=func agent()
+
+Returns a new instance of C<LWP::UserAgent> configured with C<env_proxy>.
+
+=func default_command_runner()
+
+Returns a new instance of the default implementation of 
+C<Footprintless::CommandRunner>.
+
+=func dumper(@to_dump)
+
+Prints a dump of C<@to_dump> using C<Data::Dumper> with C<Data::Dumper::Indent>
+set to 1.
+
+=func exit_due_to($reason)
+
+If C<$reason> is an instance of C<Footprintless::CommandRunner::Exception>,
+C<$reason-E<gt>exit()> will be called.  Otherwise, C<$reason> will be printed
+to C<STDERR> and C<exit(255)> will be called.
+
+=func slurp($file)
+
+Reads the entire contents of C<$file> in one gulp.
+
+=func spurt($content, $file, %options)
+
+Writes C<$content> to C<$file>.  The available options are:
+
+=over 4
+
+=item append
+
+If I<truthy>, C<$content> will be appended to C<$file> instead of overwriting.
+
+=back
+
+
